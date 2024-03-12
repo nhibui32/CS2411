9032d3c81ad172761779b2517c31fff773753863
nhibui32 <148082202+nhibui32@users.noreply.github.com>
2024-03-12 14:47:07 -0500

    MedianNum update

--- week 8/HW3_team#7.cpp
+++ week 8/HW3_team#7.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-double averNum(const int Num[], int Array_size)
+double averNum(int Num[], const int Array_size)
 {
     double sum = 0;
     for (int i=0 ; i < Array_size ; i++)
@@ -8,6 +8,19 @@ double averNum(const int Num[], int Array_size)
         sum += Num[i];
     }
     return sum / Array_size ;
+} 
+double medianNum ( int Num[], const int Array_size)
+{
+    double median = 0.0f;
+    if (Array_size % 2 == 1)
+    {
+        median = Num[(Array_size - 1) / 2];
+    }
+    else
+    {
+        median = (Num[Array_size / 2 ] + Num[Array_size / 2 - 1]) / 2.0;
+    }
+    return median;
 }
 int main()
 {
@@ -29,7 +42,8 @@ int main()
         cin >> numMovies;
         stuNum[i] = numMovies; // store the array in a temp variable, check lab #5. 
     }
-    cout << "The average number of movies watched by " << SIZE << " students is: " << averNum(stuNum, SIZE) ;
+    cout << "The average number of movies watched by " << SIZE << " students is: " << averNum(stuNum, SIZE) << endl;
+    cout << "The median number of movies watched by " << SIZE <<" students is: " << medianNum(stuNum, SIZE) << endl;
 
     return 0;
 }
